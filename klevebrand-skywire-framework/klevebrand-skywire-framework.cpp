0937d3a6c26c5578b1875e95d1c249eb2c325e27
Max Klevebrand <max.klevebrand@gmail.com>
2024-08-01 16:32:49 +0200

    Implement start scaffold for

--- klevebrand-skywire-framework/klevebrand-skywire-framework.cpp
+++ klevebrand-skywire-framework/klevebrand-skywire-framework.cpp
@@ -1,8 +1,17 @@
 #include "klevebrand-skywire-framework.h"
+#include <Arduino.h>
 #include <SoftwareSerial.h>
 
 void Skywire::start() {
     Skywire::skywire = SoftwareSerial(SKYWIRE_RX_PIN, SKYWIRE_TX_PIN);
+
+    delay(5000);
+
+    skywire.write("AT");
+
+    while(skywire.available()) {
+        String data = skywire.readString();
+    }
 }
 
 void Skywire::send() { 
--- klevebrand-skywire-framework/klevebrand-skywire-framework.h
+++ klevebrand-skywire-framework/klevebrand-skywire-framework.h
@@ -1,13 +1,19 @@
+#ifndef KLEVEBRAND_SKYWIRE_FRAMEWORK_H
+#define KLEVEBRAND_SKYWIRE_FRAMEWORK_H
+
 #include <SoftwareSerial.h>
 
-#define SKYWIRE_TX_PIN = 10
-#define SKYWIRE_RX_PIN = 11
+#define SKYWIRE_RX_PIN 10
+#define SKYWIRE_TX_PIN 11
 
 class Skywire {
-    public: 
-      void start();
-      void send();
-      SoftwareSerial skywire;
-    private:
-      void stop();
+public:
+    void start();
+    void send();
+    void stop();
+
+private:
+    static SoftwareSerial skywire;
 };
+
+#endif // KLEVEBRAND_SKYWIRE_FRAMEWORK_H
