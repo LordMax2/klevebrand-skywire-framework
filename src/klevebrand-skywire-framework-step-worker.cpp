0a01ae51f7a9ef2c0df7a67bd6e2ba113bba1150
Max Klevebrand <max.klevebrand@gmail.com>
2026-02-08 00:06:21 +0100

    Add timeout handling Cleanup constructor

--- src/klevebrand-skywire-framework-step-worker.cpp
+++ src/klevebrand-skywire-framework-step-worker.cpp
@@ -19,18 +19,21 @@ bool SkywireStepWorker::start()
 
 void SkywireStepWorker::run()
 {
-	if(millis() - last_run_timestamp < 100)
-	{
-		return;
-	}
-
 	for (int i = 0; i < step_count; i++)
 	{
 		SkywireResponseResult_t step_result = steps[i]->process();
 
-		if (!step_result.is_success)
+		// Dont go further if not successful.
+		// Also, add do not go to the next step until 100ms has passed
+		if (!step_result.is_success || millis() - steps[i]->sent_timestamp < 100)
 		{
 			break;
+		}
+
+		// If timeout on a step, force reset everything and start over
+		if (millis() - steps[i]->sent_timestamp > timeout_milliseconds)
+		{
+			resetState();
 		}
 	}
 
